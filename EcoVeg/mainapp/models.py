99ae18dbca03b9f177c0f7344915fab7bd3f28c2
satish <2019kumarsatish2019@gmail.com>
2024-06-30 22:25:05 +0530

    category product

--- EcoVeg/mainapp/models.py
+++ EcoVeg/mainapp/models.py
@@ -6,4 +6,14 @@ class Customer(models.Model):
     Contact=models.CharField(max_length=10)
     Image=models.ImageField(upload_to='images/', default='https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=')
     Password=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Name
+    
+class ProductCategory(models.Models):
+    Category=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Category
+    
 # Create your models here.
