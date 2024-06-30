90075c78f32c56e88f392121b8b5ad8c08a6ed99
satish <2019kumarsatish2019@gmail.com>
2024-06-30 22:16:22 +0530

    customer model

--- EcoVeg/mainapp/models.py
+++ EcoVeg/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Customer(models.Model):
+    Name=models.CharField(max_length=50)
+    Email=models.EmailField()
+    Contact=models.CharField(max_length=10)
+    Image=models.ImageField(upload_to='images/', default='https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=')
+    Password=models.CharField(max_length=100)
 # Create your models here.
